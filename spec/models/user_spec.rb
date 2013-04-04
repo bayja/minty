c6d5171442ad3e03aaf619b81f2695c0bddd0fa3
Sooyong Wang <wangsy@wangsy.com>
2013-04-04 10:07:50 +0900

    changed by kunha park

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 describe User do
 	let(:user) { User.create! name:'tester' }
 	let(:tweet) { Tweet.create! content: 'test content' }
+
 	it 'should add tweet to my favorites list' do
 		user.add_to_favorites tweet
 		user.favorite_tweets.should include(tweet)
