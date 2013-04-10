53c449f2968818e3294fa70b7216aeda652e35dd
Changhwan Chung <shakej@Sooyonog-Wang-ui-MacBook-Pro.local>
2013-04-10 10:22:03 +0900

    배경화면 변경 / 패스워드 추가

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,11 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by_name(params[:name])
 
-  	if user
+  	if user and user.authenticate(params[:password])
   		session[:user_id] = user.id
   		redirect_to :root
   	else
+      flash[:alert] = "Your password is Invalid"
   		redirect_to login_path 
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130404002459) do
+ActiveRecord::Schema.define(:version => 20130410001444) do
 
   create_table "favorites", :id => false, :force => true do |t|
     t.integer  "user_id"
@@ -30,13 +30,14 @@ ActiveRecord::Schema.define(:version => 20130404002459) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.text     "description"
     t.boolean  "admin"
     t.text     "address"
     t.string   "email"
     t.string   "phone"
+    t.string   "password_digest"
   end
 
   create_table "users_have_followings", :id => false, :force => true do |t|
